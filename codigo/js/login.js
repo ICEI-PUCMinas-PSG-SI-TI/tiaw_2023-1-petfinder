9757085dd96de76276d7ec1655e56593ee91838e
MiguelInacio23 <miguel.braga@sga.pucminas.br>
2023-05-03 16:48:02 -0300

    Alerta de campo vazio

--- codigo/js/login.js
+++ codigo/js/login.js
@@ -1,9 +1,37 @@
-function login() {
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+//
+
+let form = document.getElementById("formulario");
+
+function login(e) {
+  e.preventDefault();
   let email = document.getElementById("email").value;
   let senha = document.getElementById("senha").value;
-  if (email == "admin@admin.com" && senha == "123456") {
+  if (email === "admin@admin.com" && senha === "123456") {
     window.location.href = "Home.html";
-  } else {
-    alert("Favor preencher os campos corretamente!");
   }
 }
+
+form.addEventListener("submit", login);
