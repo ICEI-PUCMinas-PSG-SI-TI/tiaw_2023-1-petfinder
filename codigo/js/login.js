be61fab2f1cec416047424c5f566e907fe0d0f2f
MiguelInacio23 <miguel.braga@sga.pucminas.br>
2023-05-04 14:57:13 -0300

    Alteracao no footer,header e login

--- codigo/js/login.js
+++ codigo/js/login.js
@@ -30,7 +30,7 @@ function login(e) {
   let email = document.getElementById("email").value;
   let senha = document.getElementById("senha").value;
   if (email === "admin@admin.com" && senha === "123456") {
-    window.location.href = "Home.html";
+    window.location.href = "Perfil.html";
   }
 }
 
