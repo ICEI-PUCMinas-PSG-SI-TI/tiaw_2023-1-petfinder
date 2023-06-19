a54ddbddb64ec60f3d61d68821bc16117eaecd56
MiguelInacio23 <miguel.braga@sga.pucminas.br>
2023-06-19 15:29:52 -0300

    Cadastro Atualizado

--- codigo/cadastro.js
+++ codigo/cadastro.js
@@ -1,54 +1,35 @@
-function ler() {
-    let stringDados = localStorage.getItem('db');
-    let objdados = [];
-
-    return objdados;
-
+let dadosObject = [];
+function salvar() {
+  localStorage.setItem("Cadastro", JSON.stringify(dadosObject));
 }
-
-//let objdados = JSON.parse(localStorage.getItem("db"))
-
-function salvar(stringDados) {
-
-    localStorage.setItem('db', JSON.stringify(stringDados));
-}
-
-
 function incluir() {
-    let objdados = ler();
-    let xemail = document.getElementById("Cemail").value;
-    let xsenha = document.getElementById("Csenha").value;
-    let xendereco = document.getElementById("Cendereco1").value;
-    let xendereco2 = document.getElementById("Cendereco2").value;
-    let xtelefone = document.getElementById("Ctelefone").value;
-    let xcidade = document.getElementById("Ccidade").value;
-    let xestado = document.getElementById("Cestado").value;
-    let xcep = document.getElementById("Ccep").value;
+  //let objdados = ler();
+  let xnome = document.getElementById("Cemail").value;
+  let xsenha = document.getElementById("Csenha").value;
+  let xendereco = document.getElementById("Cendereco1").value;
+  let xendereco2 = document.getElementById("Cendereco2").value;
+  let xtelefone = document.getElementById("Ctelefone").value;
+  let xcidade = document.getElementById("Ccidade").value;
+  let xestado = document.getElementById("Cestado").value;
+  let xcep = document.getElementById("Ccep").value;
 
-    let NovoContato = {
-        nome: xemail,
-        senha: xsenha,
-        endereco: xendereco,
-        endereco2: xendereco2,
-        telefone: xtelefone,
-        cidade: xcidade,
-        estado: xestado,
-        cep: xcep
-    }
-    objdados.push(NovoContato);
-    salvar(objdados);
-    imprime();
+  let NovoContato = {
+    email: xnome,
+    senha: xsenha,
+    endereco: xendereco,
+    endereco2: xendereco2,
+    telefone: xtelefone,
+    cidade: xcidade,
+    estado: xestado,
+    cep: xcep,
+  };
 
-
+  dadosObject.push(NovoContato);
+  salvar();
 }
+document.getElementById("Botao").addEventListener("click", incluir);
 function imprime() {
-
-    let strhtml = ""
-    let objdados = ler();
-
-
+  let strhtml = "";
+  let objdados = ler();
 }
-document.getElementById("Botao").addEventListener('click', incluir);
-
-
-
+document.getElementById("Botao").addEventListener("click", incluir);
